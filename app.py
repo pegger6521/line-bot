ed20e4bf12334d0ee91a702991f0b5b2075b3dd6
Peggy LAN <pegger6521@gmail.com>
2022-05-14 00:02:49 +0800

    add sticker function

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
@@ -47,6 +47,17 @@ def handle_message(event):
     msg = event.message.text
     r = 'Sorry, 您說啥麼'
 
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+        return
+
     if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '你吃飯了嗎':
@@ -59,7 +70,6 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
-
 
 if __name__ == "__main__":
     app.run()
