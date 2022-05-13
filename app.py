898e41101ffbfcf380655fca03b1b52749934968
Peggy LAN <pegger6521@gmail.com>
2022-05-13 23:51:03 +0800

    update

--- app.py
+++ app.py
@@ -45,12 +45,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = 'Sorry, 您說什麼'
-    
-    if msg == 'hi':
-        r = 'hi'
-    elif msg == '你吃飯了嗎?':
+    r = 'Sorry, 您說啥麼'
+
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
         r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位, 是嗎?'
 
     line_bot_api.reply_message(
         event.reply_token,
